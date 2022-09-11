949db166f33282e8172d4199a2df17730f23f087
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2022-09-11 10:04:58 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <input type="color" />
+      <select>
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+      </select>
+    </div>
+  )
+}
+
+export default App
