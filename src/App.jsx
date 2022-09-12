a95ba1a5bf0c6ec875e1a41fa511e3315f4c1c53
AndyTBurke <107972213+AndyTBurke@users.noreply.github.com>
2022-09-11 20:02:05 -0500

    Functionality Complete
    App should be fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,72 @@
 import { useState, useEffect } from 'react'
 
 function App() {
+  const defaultState = {
+    pickedColor: "#334433",
+    mode: "monochrome",
+    recColor1: "red",
+    recColor2: "green",
+    recColor3: "blue",
+    recColor4: "purple",
+    refreshCount: 0
+  }
+
+  const [state, setState] = useState(defaultState)
+
+  const modes = ["monochrome", "monochrome-dark", 
+                "monochrome-light", "analogic", "complement", 
+                "analogic-compliment", "triad", "quad"]
+
+  const modeChoices = modes.map(mode => <option key={mode} value={mode}>{mode}</option>)
+
+  useEffect(() => {
+    fetch(`https://www.thecolorapi.com/scheme?hex=${state.pickedColor.slice(1,7)}&mode=${state.mode}&count=4`)
+      .then(res => res.json())
+      .then(data => (
+        setState(prevState => ({
+          ...prevState,
+          recColor1: data.colors[0].hex.value,
+          recColor2: data.colors[1].hex.value,
+          recColor3: data.colors[2].hex.value,
+          recColor4: data.colors[3].hex.value
+        }))))
+    }, [state.refreshCount])
+
+  function setPickedColor(color) {
+    setState((prevState) => ({
+      ...prevState,
+      pickedColor: color
+    }))
+  }
+
+  function setMode(newMode) {
+    setState((prevState) => ({
+      ...prevState,
+      mode: newMode
+    }
+    ))
+  }
+  
+  function newColorways() {
+    setState(prevState => ({
+      ...prevState,
+      refreshCount: prevState.refreshCount + 1
+    }))
+  }
 
   return (
     <div className="App">
-      <input type="color" />
-      <select>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
+      <h1>COLORWAY GENERATOR</h1>
+      <input className="color-picker" type="color" value={state.pickedColor} onChange={event => setPickedColor(event.target.value)}/>
+      <select className="type-picker" onChange={(event) => setMode(event.target.value)} value={state.mode}>
+        {modeChoices}
       </select>
+      <button onClick={() => newColorways()}>GET COLORWAY</button>
+      <div className="color-row color-selection" style={{backgroundColor: state.pickedColor}}></div>
+      <div className="color-row color-pick-1" style={{backgroundColor: state.recColor1}}></div>
+      <div className="color-row color-pick-2" style={{backgroundColor: state.recColor2}}></div>
+      <div className="color-row color-pick-3" style={{backgroundColor: state.recColor3}}></div>
+      <div className="color-row color-pick-4" style={{backgroundColor: state.recColor4}}></div>
     </div>
   )
 }
